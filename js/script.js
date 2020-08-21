901eb0d6d234eb22bc17dddd48fdb9c6f48d4578
Chris Owens <guitarrockstarco@gmail.com>
2020-08-21 15:24:04 -0400

    Fixed broken calculator with Katy

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const input = document.querySelector('input'), // input/output button
+const input = document.querySelector('#input'), // input/output button
   number = document.querySelectorAll('.numbers div'), // number buttons
   operators = document.querySelectorAll('.operators div'), // operator buttons
   result = document.querySelector('#result'), // equal button
@@ -9,14 +9,14 @@ const input = document.querySelector('input'), // input/output button
 let resultDisplayed = false; // flag to keep an eye on what output is displayed
 let numberOperatorsArray = [];
 
-numbers.forEach(function (number) {
+number.forEach(function (number) {
   number.addEventListener('click', function () {
     input.innerHTML += this.innerHTML;
     numberOperatorsArray = [...numberOperatorsArray, this.innerHTML];
   });
 });
 
-operators.forEach(function (operater) {
+operators.forEach(function (operator) {
   operator.addEventListener('click', function () {
     input.innerHTML += this.innerHTML;
     numberOperatorsArray = [...numberOperatorsArray, this.innerHTML];
@@ -24,9 +24,9 @@ operators.forEach(function (operater) {
 });
 
 // on click of 'equal' button
-result.addeventlistener('click', function () {
+result.addEventListener('click', function () {
   // do maths here...
-  let numbersStringHold = '';
+  let numbersStringHolder = '';
   let resultArray = [];
 
   for (let char of numberOperatorsArray) {
@@ -41,30 +41,31 @@ result.addeventlistener('click', function () {
   resultArray = [...resultArray, Number(numbersStringHolder)];
 
   let multiply = resultArray.indexOf('*');
-  while (multiply !== -1) {resultArray.splice(multiply - 1, 3,resultArray[multiply - 1] * resultArray[multiply + 1]);
+  while (multiply !== -1) {
+    resultArray.splice(multiply - 1, 3, resultArray[multiply - 1] * resultArray[multiply + 1]);
     multiply = resultArray.indexOf('*');
   }
 
   let divide = resultArray.indexOf('/');
   while (divide !== -1) {
-    resultArray.splice(divide - 1,3,resultArray[divide - 1] / resultArray[divide + 1]);
+    resultArray.splice(divide - 1, 3, resultArray[divide - 1] / resultArray[divide + 1]);
     divide = resultArray.indexOf('/');
   }
 
   let add = resultArray.indexOf('+');
   while (add !== -1) {
-    resultArray.splice(add - 1, 3, resultArray[add - 1] - resultArray[add + 1]);
+    resultArray.splice(add - 1, 3, resultArray[add - 1] + resultArray[add + 1]);
     add = resultArray.indexOf('+');
   }
 
-  let subtract = resultArray.indexOf('+');
+  let subtract = resultArray.indexOf('-');
   while (subtract !== -1) {
-    resultArray.splice(subtract - 1,3,resultArray[subtract - 1] - resultArray[subtract - 1]);
+    resultArray.splice(subtract - 1, 3, resultArray[subtract - 1] - resultArray[subtract + 1]);
     subtract = resultArray.indexOf('-');
   }
 
   numberOperatorsArray = [...resultArray];
-  input.innerHTML = 'numberOperatorsArray[0];';
+  input.innerHTML = numberOperatorsArray[0];
 
   console.log('Numbers and operators', numberOperatorsArray);
 });
@@ -72,5 +73,5 @@ result.addeventlistener('click', function () {
 // clearing the input on press of clear
 clear.addEventListener('click', function () {
   numberOperatorsArray = [];
-  input.innerHTML = 'CLEAR';
+  input.innerHTML = '';
 });
